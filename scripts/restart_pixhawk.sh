15cb763bf831e9b073a0276fe565f73788e74e24
Lovro Markovic <lovro.markovic@fer.hr>
2021-01-12 16:00:16 +0100

    Add namespace to restart_pixhawk.sh

--- scripts/restart_pixhawk.sh
+++ scripts/restart_pixhawk.sh
@@ -1,3 +1,1 @@
-rosservice call /red/mavros/cmd/command "{broadcast: false, command: 246, confirmation: 0, param1: 1.0, param2: 0.0, param3: 0.0}"
-
-
+rosservice call /$UAV_NAMESPACE/mavros/cmd/command "{broadcast: false, command: 246, confirmation: 0, param1: 1.0, param2: 0.0, param3: 0.0}"
